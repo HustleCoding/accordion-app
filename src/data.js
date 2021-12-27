3591f012f06515acbe0c59eb67c7fbc70b2b2f46
HustleCoding <f.dobinciuc7@gmail.com>
2021-12-27 21:42:03 +0200

    [introducing useState hook]

--- src/data.js
+++ src/data.js
@@ -1,15 +1,18 @@
 const items = [
   {
-    title: "What is React?",
-    content: "React is a Javascript framework",
+    title: "What is Life?",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
   },
   {
-    title: "Why use React?",
-    content: "React is a favorite library among engineers",
+    title: "Why use Life?",
+    content:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
   },
   {
-    title: "How do you use React?",
-    content: "You use React by creating components",
+    title: "How do you use Life?",
+    content:
+      "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
   },
 ];
 
