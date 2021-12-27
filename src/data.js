11d9a425221295ff492f1a6526ec16107d25c3b2
HustleCoding <f.dobinciuc7@gmail.com>
2021-12-27 10:34:33 +0200

    [accordion component added + data file]

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const items = () => [
+  {
+    title: "What is React?",
+    content: "React is a Javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+export default items;
