7abf9177606c00d73b4ca5fc0aed16465a04d697
HustleCoding <f.dobinciuc7@gmail.com>
2021-12-28 11:11:02 +0200

    [master branch tested]

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import Accordion from "./components/Accordion";
 
 const App = () => {
-  return <div>{/* <Accordion /> */}</div>;
+  return (
+    <div>
+      <Accordion />{" "}
+    </div>
+  );
 };
 
 export default App;
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,30 +1,31 @@
-// import React, { useState } from "react";
-// import items from "../data";
+import React, { useState } from "react";
+import items from "../data";
 
-// const Accordion = () => {
-//   const [activeIndex, setActiveIndex] = useState(null);
-//   // helper function
-//   const onTitleClick = (index) => {
-//     setActiveIndex(index);
-//   };
+const Accordion = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
 
-//   const renderedItems = items.map((item, index) => {
-//     const active = index === activeIndex ? "active" : " ";
+  // helper function
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
 
-//     return (
-//       <React.Fragment key={item.title}>
-//         <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
-//           <i className="dropdown icon"></i>
-//           {item.title}
-//         </div>
-//         <div className={`content ${active}`}>
-//           <p>{item.content}</p>
-//         </div>
-//       </React.Fragment>
-//     );
-//   });
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : " ";
 
-//   return <div className="ui styled accordion">{renderedItems}</div>;
-// };
+    return (
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
 
-// export default Accordion;
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
+
+export default Accordion;
