201294077c7b2b378d7f00c872d3e5c180d2d3f4
HustleCoding <f.dobinciuc7@gmail.com>
2021-12-28 11:16:11 +0200

    [reset the app]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Accordion from "./components/Accordion";
 
 const App = () => {
-  return (
-    <div>
-      <Accordion />{" "}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
