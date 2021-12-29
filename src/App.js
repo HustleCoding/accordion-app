7deffcd36949cec423a66381c49e077c144c1dff
HustleCoding <f.dobinciuc7@gmail.com>
2021-12-29 11:37:28 +0200

    [testing the new branch]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Search from "./components/Search";
 
 const App = () => {
-  return (
-    <div>
-      <Search />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
