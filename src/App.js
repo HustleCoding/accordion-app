0155a226063f406c6739d87a4f1abed61fcb917a
FlorinDobinciuc <florin.dobinciuc@thinslices.com>
2022-01-06 22:44:15 +0200

    [testing]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Theme from "./components/Theme";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Theme />
+    </div>
+  );
 };
 
 export default App;
